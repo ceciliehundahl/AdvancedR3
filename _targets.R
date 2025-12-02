b083259cb0ba91c9b866e6622c7531cc005cd440
Cecilie Hundahl Johnsen <ceciliehundahl@gmail.com>
2025-12-02 15:26:20 +0100

    Setup targets

--- _targets.R
+++ _targets.R
@@ -5,11 +5,10 @@
 
 # Load packages required to define the pipeline:
 library(targets)
-# library(tarchetypes) # Load other packages as needed.
 
 # Set target options:
 tar_option_set(
-  packages = c("tibble") # Packages that your targets need for their tasks.
+  # packages = c("tibble") # Packages that your targets need for their tasks.
   # format = "qs", # Optionally set the default storage format. qs is fast.
   #
   # Pipelines that take a long time to run may benefit from
@@ -46,17 +45,16 @@ tar_option_set(
 
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
-# tar_source("other_functions.R") # Source other scripts as needed.
 
 # Replace the target list below with your own:
 list(
   tar_target(
-    name = data,
-    command = tibble(x = rnorm(100), y = rnorm(100))
-    # format = "qs" # Efficient storage for general data objects.
+    name = file,
+    command = "data/lipidomics.csv",
+    format = "file"
   ),
   tar_target(
-    name = model,
-    command = coefficients(lm(y ~ x, data = data))
+    name = lipidomics,
+    command = readr::read_csv(file, show_col_types = FALSE)
   )
 )
